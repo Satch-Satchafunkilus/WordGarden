0398fb5647654ade3b1e48f751761a18d96e3795
Tushar Munge <63478179+Satch-Satchafunkilus@users.noreply.github.com>
2025-06-07 09:37:24 -0500

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Tushar Munge on 6/7/25.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
